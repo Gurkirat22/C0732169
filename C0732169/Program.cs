7551e2caa0d6dc810963b04191466741e56017e7
visualstudio <visualstudio@122-PC07>
2019-01-18 21:08:19 -0500

    Class program

--- C0732169/Program.cs
+++ C0732169/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace C0732169
 {
+
     class Program
+
+
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 20);
         }
     }
+
+    class StudentInformation
+    {
+        public string Name;
+        public string StudentNumber;
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(string aName, double cGPA, int dAttandence)
+        {
+
+            Name = aName;
+            GPA = cGPA;
+            Attendance = dAttandence;
+
+        }
+
+
+    }
+
 }
